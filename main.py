3e3d16f4c153b79fe2de959414eb20a91cd91303
Nathan Ganser <me@nathanganser.com>
2020-04-13 08:49:04 +0000

    Setting up the core app

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from flask import Flask
 app = Flask('app')
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def welcome():
+  return 'Welcome page - Sync Contacts button'
+
+
+@app.route('/dashboard')
+def dashboard():
+  return 'Main Dashboard'
 
 app.run(host='0.0.0.0', port=8080)
